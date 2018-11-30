dedc83865deb869c4723abdcbcd59d249c698d5c
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-11-30 23:14:08 +0300

    upd task3

--- task3.hs
+++ task3.hs
@@ -1,8 +1,8 @@
 import Data.List
 
 revRange :: (Char, Char) -> [Char]
-revRange (a, b) = unfoldr fun (b, False)
-    where fun (x, isEnd) 
-        | x < a || isEnd = Nothing
-        | x > a = Just (x, (pred x, False))
-        | x == a = Just (x, (x, True))
+revRange = unfoldr fun
+fun (first, last)
+    | first > last = Nothing
+    | last == minBound = Just (last, (succ first, last))
+    | otherwise = Just (last, (first, pred last))

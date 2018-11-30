aeefa3a199707ce50f1ad2cc063f15a110700fcc
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-11-30 23:49:32 +0300

    ad task2

--- task2.hs
+++ task2.hs
@@ -1,8 +1,8 @@
 indices :: [a] -> [(Integer, a)]
 indices l = zip [0..] l
 
---zeroBy :: Monoid a => [a] -> (a -> Bool) -> [a]
-
+zeroBy :: Monoid a => [a] -> (a -> Bool) -> [a]
+zeroBy list predicate = map (\a -> if predicate a then mempty else a) list
 
 triplewiseSum :: [Integer] -> [Integer] -> [Integer] -> [Integer]
 triplewiseSum l1 l2 l3 = zipWith (+) l1 (zipWith (+) l2 l3)

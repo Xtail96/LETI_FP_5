4f430460b2c9142dc519792c2a15bbcf07b5b462
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2018-11-30 22:19:18 +0300

    2.3

--- task2.hs
+++ task2.hs
@@ -1,2 +1,8 @@
 indices :: [a] -> [(Integer, a)]
 indices l = zip [0..] l
+
+--zeroBy :: Monoid a => [a] -> (a -> Bool) -> [a]
+
+
+triplewiseSum :: [Integer] -> [Integer] -> [Integer] -> [Integer]
+triplewiseSum l1 l2 l3 = zipWith (+) l1 (zipWith (+) l2 l3)
